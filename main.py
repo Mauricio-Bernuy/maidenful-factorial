cbf13b2fea909753075120c9c4a26dc5c62a64b9
Mauricio Bernuy <mauricio.bernuy@utec.edu.pe>
2022-06-07 21:36:30 -0500

    finished with binet

--- main.py
+++ main.py
@@ -18,6 +18,23 @@ def factorial(n=None):
 
         return render_template('a.html', n=factorial, t = time)
 
+@app.route("/fibonacci/<n>")
+def fibonacci(n=None):
+        n = int(n)
+        start = timer()
+        
+        # binet formula
+        fibonacci =  round((math.pow(1.618033988749895, n) - math.pow(-0.6180339887498949, n)) / 2.23606797749979, 5)
+                
+        
+
+        end = timer()
+
+        time = end-start
+
+        return render_template('b.html', n=fibonacci, t = time)
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
